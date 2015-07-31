98d9a5394827f316e9d6d807254677e194babf7b
Simon <s2imon@users.noreply.github.com>
2015-07-31 14:59:01 +0200

    Create if_else_multilang_wordpress.php

---
+++ if_else_multilang_wordpress.php
@@ -0,0 +1,33 @@
+<?php
+if (ICL_LANGUAGE_CODE == 'en') { ?>
+
+<!-- inserire qui il codice di iubenda in inglese -->
+
+<?php
+} else if (ICL_LANGUAGE_CODE == 'es') { ?>
+
+<!-- inserire qui il codice di iubenda in spagnolo "es" -->
+
+<?php
+} else if (ICL_LANGUAGE_CODE == 'fr') {?>
+
+<!-- inserire qui il codice di iubenda in francese "fr" -->
+
+<?php
+} else if (ICL_LANGUAGE_CODE == 'de') {?>
+
+<!-- inserire qui il codice di iubenda in tedesco "de" -->
+
+<?php
+} else if (ICL_LANGUAGE_CODE == 'it') {?>
+
+<!-- inserire qui il codice di iubenda in italiano "it" -->
+
+<?php
+} else if (ICL_LANGUAGE_CODE == 'pt-BR') {?>
+
+<!-- inserire qui il codice di iubenda in portoghese brasiliano "pt-BR" -->
+
+<?php
+}
+?>
