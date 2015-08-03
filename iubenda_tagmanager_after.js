1bfc7a19dbdcd897b562d0e5f35e94742acae449
Simon <s2imon@users.noreply.github.com>
2015-08-03 16:45:05 +0200

    Create iubenda_tagmanager_after.js

---
+++ iubenda_tagmanager_after.js
@@ -0,0 +1,15 @@
+<script type="text/javascript">
+var _iub = _iub || [];
+_iub.csConfiguration = {
+  cookiePolicyId: 254316,
+  callback: {
+    onConsentGiven: function(){
+      dataLayer.push({'event': 'iubenda_consent_given'});
+    }
+  }
+};
+(function (w, d) {
+var loader = function () { var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src = "//cdn.iubenda.com/cookie_solution/iubenda_cs.js"; tag.parentNode.insertBefore(s, tag); };
+if (w.addEventListener) { w.addEventListener("load", loader, false); } else if (w.attachEvent) { w.attachEvent("onload", loader); } else { w.onload = loader; }
+})(window, document);
+</script>
