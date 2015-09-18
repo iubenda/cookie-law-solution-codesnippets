a16bea399fab7155e379dde8aef2822beba24468
darioAtIubenda <dario@iubenda.com>
2015-09-18 09:48:21 +0200

    Create google_search_before.js

---
+++ google_search_before.js
@@ -0,0 +1,13 @@
+<script type="text/javascript">
+      (function() {
+          var cx = '006879106247619883145:quvjfulgdfo';
+          var gcse = document.createElement('script');
+          gcse.type = 'text/javascript';
+          gcse.async = true;
+          gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+                  '//cse.google.com/cse.js?cx=' + cx;
+          var s = document.getElementsByTagName('script')[0];
+          s.parentNode.insertBefore(gcse, s);
+      })();
+  </script>
+<gcse:search></gcse:search>
