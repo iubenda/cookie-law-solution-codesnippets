e2ebc550934b2a29045d53d4c9e38be5f981c0ed
Simon Schmid <zimonschmid@gmail.com>
2015-09-30 11:24:20 +0200

    Add class usage php

---
+++ iubendaClassUsage.php
@@ -0,0 +1,18 @@
+<?php
+    include_once 'simple_html_dom.php';
+    include_once 'iubenda.class.php';
+    // Check if the user has already given consent
+    if(!Page::consent_given()){
+     	 // Here you should pass the content of the page, this is just an example
+     	 // using file_get_contents(url) to have a real web page
+     	 $content = YOUR_PAGE_CONTENT // with iubenda js banner included
+     	 // Istantiate new Page with content
+     	 $page = new Page($content);
+     	 // Parse and convert content
+     	 $page->parse();
+     	 // Print the new page
+     	 echo $page->get_converted_page();
+    }else{
+   	   echo 'Consent already given.. print the page without instantiate and parse';
+    }
+?>
