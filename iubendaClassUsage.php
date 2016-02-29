2d20c01bc909c0d8a7be22c7c80ffeea12df3f61
Simon <s2imon@users.noreply.github.com>
2016-02-29 11:23:28 +0100

    updating according to the changes on iubenda-cookie-class
    https://github.com/iubenda/iubenda-cookie-class/blob/master/usage.php

--- iubendaClassUsage.php
+++ iubendaClassUsage.php
@@ -1,30 +1,39 @@
+<?php
 // the "$html" parameter must contain the content of the web page with the iubenda JavaScript banner/policy included
-
+ 
 function iubenda_system($html)
 {
-        if(empty($html)) return;
-
-        /*
-         * Separator
-        */
-
-        if(!function_exists("file_get_html")) {
-            require_once("simple_html_dom.php");
-        }
-
-        require_once("iubenda.class.php");
-
-        /*
-         * Separator
-        */
-
-        if(!Page::consent_given() && !Page::bot_detected()) {
-                $page = new Page($html);
-                $page->parse();
-                $html = $page->get_converted_page();
-        }
-
-        /* Finished */
-
-        return $html;
+    if(empty($html)) return;
+       
+    /*
+     * Separator
+    */
+ 
+    if(!function_exists("file_get_html")) {
+        require_once("simple_html_dom.php");
+    }
+ 
+    require_once("iubenda.class.php");
+ 
+    /*
+     * Separator
+    */
+ 
+    if(!Page::consent_given() && !Page::bot_detected()) {
+        $page = new Page($html);
+        $page->parse();
+        $html = $page->get_converted_page();
+    }
+ 
+    /* Finished */
+ 
+   	return $html;
 }
+/*
+ *
+ * Example:
+ *
+ * echo iubenda_system("<html> ...content... </html>");
+ *
+*/
+?>
