4aa3e6f02e197c2b513cfd1c3901a76caff1d878
Dario Gasparro <dario.gas@gmail.com>
2016-07-01 15:47:35 +0200

    Create cookie_law.php

---
+++ wordpress/cookie_law.php
@@ -0,0 +1,21 @@
+<?php
+    function iubenda_system($html)
+    {
+        if(!function_exists("file_get_html")) {
+            require_once("simple_html_dom.php");
+        }
+
+        require_once("iubenda.class.php");
+
+        if(!Page::consent_given() && !Page::bot_detected())
+        {
+            $page = new Page($html);
+            $page->parse();
+            $html = $page->get_converted_page();
+        }
+
+        return $html;
+    }
+    
+    ob_start("iubenda_invoke");
+?>
