beee11d2d988e1491b8accbfa08c08b2bf9b2fa1
Dario Gasparro <dario.gas@gmail.com>
2016-07-22 16:25:26 +0200

    Create after.js

---
+++ snippet/kiss_metrics/after.js
@@ -0,0 +1,15 @@
+<!-- KISSmetrics tracking snippet -->
+<script type="text/plain"  class="_iub_cs_activate">
+        var _kmq = _kmq || [];
+        var _kmk = _kmk || 'e76b907358e7b934865c0d9319153da8a43008c1';
+        function _kms(u){
+          setTimeout(function(){
+              var d = document, f = d.getElementsByTagName('script')[0],
+                      s = d.createElement('script');
+              s.type = 'text/javascript'; s.async = true; s.src = u;
+              f.parentNode.insertBefore(s, f);
+          }, 1);
+        }
+        _kms('//i.kissmetrics.com/i.js');
+        _kms('//scripts.kissmetrics.com/' + _kmk + '.2.js');
+</script>
