1f9703361863ab3b0cd73a3a8aa1898007338723
Simon <s2imon@users.noreply.github.com>
2016-10-20 12:21:40 +0200

    removing the actual real id

--- embedding_privacy_policy/embed_cookie_policy.js
+++ embedding_privacy_policy/embed_cookie_policy.js
@@ -1,3 +1,3 @@
-<a class="iubenda-white iub-body-embed iubenda-embed" title="Cookie Policy" href="//www.iubenda.com/privacy-policy/7842289/cookie-policy">Cookie Policy</a><script type="text/javascript">
+<a class="iubenda-white iub-body-embed iubenda-embed" title="Cookie Policy" href="//www.iubenda.com/privacy-policy/abcDXYZ/cookie-policy">Cookie Policy</a><script type="text/javascript">
 (function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src = "//cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);
 </script>
