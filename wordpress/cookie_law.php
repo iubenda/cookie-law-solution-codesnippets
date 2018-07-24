e35345f718e21f9d5006988874410d921e0b2365
abbieclement <40836347+abbieclement@users.noreply.github.com>
2018-07-24 17:14:15 +0200

    Update cookie_law.php

--- wordpress/cookie_law.php
+++ wordpress/cookie_law.php
@@ -5,17 +5,16 @@
         }
  
         require_once( "iubenda.class.php" );
-        /*For experimental "faster" class add this line here: 
-        require_once( "iubenda.class.faster.php" ); */
 
         $page = new iubendaPage( $html );
  
         if (! $page->consent_given() && ! $page->bot_detected() ) {
-            $page->parse();/*For experimental class, replace this line with: $parser = new iubendaFaster(); */
-            $html = $page->get_converted_page();/*For experimental class, replace this line with: $html = $parser -> isParse( mb_convert_encoding( $content, "HTML-ENTITIES", "UTF-8" ) ); */
+            $page->parse();
+            $html = $page->get_converted_page();
         }
  
         return $html;
     }
     
-    ob_start( "iubenda_system" );?>
+    ob_start( "iubenda_system" );
+?>
