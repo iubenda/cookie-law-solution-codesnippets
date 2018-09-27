51f3bcdbec3747934a54ad4a1f76bd3f57ee85a4
abbieclement <40836347+abbieclement@users.noreply.github.com>
2018-09-27 14:21:02 +0200

    Update cookie_law.php
    Updated PHP class integration example (with unified class methods)

--- wordpress/cookie_law.php
+++ wordpress/cookie_law.php
@@ -1,20 +1,15 @@
+```php
 <?php
-    function iubenda_system( $html ) {
-        if ( ! function_exists( "file_get_html" ) ) {
-            require_once( "simple_html_dom.php" );
-        }
- 
-        require_once( "iubenda.class.php" );
-
-        $page = new iubendaPage( $html );
- 
-        if (! $page->consent_given() && ! $page->bot_detected() ) {
-            $page->parse();
-            $html = $page->get_converted_page();
-        }
- 
-        return $html;
-    }
-    
-    ob_start( "iubenda_system" );
-?>
+function iubenda_system( $html, $type = 'page' ) {
+	if ( empty( $html ) )
+		return;
+	require_once( 'iubenda.class.php' );
+	// separator
+	if ( ! iubendaParser::consent_given() && ! iubendaParser::bot_detected() ) {
+		$iubenda = new iubendaParser( $html, array( 'type' => in_array( $type, array( 'page', 'faster' ), true ) ? $type : 'page' ) );
+		$html = $iubenda->parse();
+	}
+	// finished
+	return $html;
+}
+```
